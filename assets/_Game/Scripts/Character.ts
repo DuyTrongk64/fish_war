ed5866a859ba0db18b67664406b5635ddb423f38
DuyTrongk64 <duytrongk64@gmail.com>
2023-05-29 01:21:31 +0700

    none

---
+++ assets/_Game/Scripts/Character.ts
@@ -0,0 +1,17 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    
+    //khởi tạo
+    public onInit(){
+        
+    }
+
+    protected onDeath(){
+        
+    }
+
+    
+}
