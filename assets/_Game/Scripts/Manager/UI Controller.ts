0d7faf0383fc6641057605c21c5a8b348dd47878
DuyTrongk64 <duytrongk64@gmail.com>
2023-06-28 00:55:52 +0700

    df

--- assets/_Game/Scripts/Manager/UI Controller.ts
+++ assets/_Game/Scripts/Manager/UI Controller.ts
@@ -1,43 +1,52 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class UI_Controller extends cc.Component {
 
     @property(cc.Node)
     blackBG: cc.Node = null;
 
     @property(cc.Node)
     arrowOx: cc.Node = null;
-    
+
     @property(cc.Node)
     arrowOy: cc.Node = null;
 
     private time: number = 0;
     private delay_time: number = 2;
 
-    start () {
-        
+    onLoad() {
+        this.blackBG.active = false;
+        this.arrowOx.active = false;
+        this.arrowOy.active = false;
     }
 
-    update(dt){
-        this.time+=dt;
-        if(this.time>=this.delay_time){
-            this.scheduleOnce(() => {  /// hàm scheduleOnce(()=>{ "thực hiện gì" }, trong vòng bao nhiêu giây) // gọi hàm callback trong khoảng bao nhiêu giây
-                this.blackBG.zIndex = 3;
-                this.blackBG.active = true;
-                //console.log(this.blackBG.active);
-            }, 0.5);
-            this.scheduleOnce(() => {
-                this.arrowOx.active = true;
-                console.log(this.arrowOx.active);
+    start() {
 
-                this.arrowOy.active = true;
-            }, 1.4);
-            this.scheduleOnce(() => {
-                this.blackBG.active = false;
-                this.arrowOx.active = false;
-                this.arrowOy.active = false;
-            }, 0);
+    }
+
+    runAnimation(){
+        this.scheduleOnce(() => {  /// hàm scheduleOnce(()=>{ "thực hiện gì" }, trong vòng bao nhiêu giây) // gọi hàm callback trong khoảng bao nhiêu giây
+            this.blackBG.zIndex = 3;
+            this.blackBG.active = true;
+            //console.log(this.blackBG.active);
+        }, 0.5);
+        this.scheduleOnce(() => {
+            this.arrowOx.active = true;
+            console.log(this.arrowOx.active);
+
+            this.arrowOy.active = true;
+        }, 1.4);
+        this.scheduleOnce(() => {
+            this.blackBG.active = false;
+            this.arrowOx.active = false;
+            this.arrowOy.active = false;
+        }, 0);
+    }
+    update(dt) {
+        this.time += dt;
+        if (this.time >= this.delay_time) {
+            this.runAnimation();
         }
     }
 }
