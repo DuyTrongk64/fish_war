ca14d2538f35b20cc0d490040fc642c0be0f2937
DuyTrongk64 <duytrongk64@gmail.com>
2023-06-29 02:27:27 +0700

    finish fish joystick

--- assets/_Game/Scripts/Start_fish.ts
+++ assets/_Game/Scripts/Start_fish.ts
@@ -10,7 +10,7 @@ export default class NewClass extends cc.Component {
     private delay_time: number = 4;
 
     public Joystick_Vector: cc.Vec2;
-
+    private startJoystickPos: cc.Vec2;
     private isActive: boolean;
 
 
@@ -25,26 +25,26 @@ export default class NewClass extends cc.Component {
         this.node.on(cc.Node.EventType.TOUCH_MOVE, this.on_stick_move, this);
         this.node.on(cc.Node.EventType.TOUCH_END, this.on_stick_end, this);
         this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.on_stick_end, this);
+
+        this.isActive = false;
     }
 
     private on_stick_start(event: cc.Touch){
-        let touch_pos = event.getLocation();
-        let local_touch_pos = this.node.convertToNodeSpaceAR(touch_pos);
-        
-        //this.node.setPosition(local_touch_pos);
-        this.Joystick_Vector = local_touch_pos;
+        this.startJoystickPos = this.node.getPosition();
+        console.log(this.startJoystickPos);
+        //this.startJoystickPos = this.node.parent.convertToNodeSpaceAR(this.node.getPosition());
+        this.isActive = true;
     }
 
     private on_stick_move(event: cc.Touch){
         let touch_pos = event.getLocation();
-        let local_touch_pos = this.node.convertToNodeSpaceAR(touch_pos);
-        this.node.setPosition(local_touch_pos);
-        this.Joystick_Vector = local_touch_pos;
+        let local_touch_pos = this.node.parent.convertToNodeSpaceAR(touch_pos);
+        this.node.setPosition(local_touch_pos.x,this.startJoystickPos.y);
+        //this.Joystick_Vector = local_touch_pos;
     }
 
     private on_stick_end(event: cc.Touch){
-        this.Joystick_Vector = cc.Vec2.ZERO;
-        this.node.setPosition(cc.Vec2.ZERO);
+        
     }
     
 
@@ -52,6 +52,10 @@ export default class NewClass extends cc.Component {
         this.time += dt;
         if (this.time >= this.delay_time && this.time < 5) {
             this.fish_df.play();
+        }
+
+        if(this.isActive){
+            this.fish_df.pause();
         }
     }
 
